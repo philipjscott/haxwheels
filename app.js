86da9c00367f3fd7ae5a03ada4f840244960daca
Philip Scott <pscott@zeptohost.com>
2018-07-14 09:42:50 -0400

    feat(3): Add Node.js static file server (#5)

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, './static')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './static/index.html'))
+})
+
+module.exports = app
