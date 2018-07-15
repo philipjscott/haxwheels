6e04b1ddc7dce2da082cd3af514b03955c4ced62
Philip Scott <pscott@zeptohost.com>
2018-07-15 13:52:24 -0400

    feat(18): Render a sprite using PixiJS

--- client/index.js
+++ client/index.js
@@ -2,22 +2,40 @@
 
 import * as Colyseus from 'colyseus.js'
 import { $ } from './util'
+import * as PIXI from 'pixi.js'
+import * as assets from './assets'
 
+console.log(assets, 'foo')
+
+const app = new PIXI.Application()
 const client = new Colyseus.Client('ws://localhost:8080')
 const room = client.join('race')
-
-console.log('Client JavaScript loaded!')
+const directions = ['up', 'down', 'left', 'right']
 
 room.listen('players/:id/position/:attribute', (change) => {
   console.log(change.path.id, change.path.attribute, change.value)
 })
-
-const directions = ['up', 'down', 'left', 'right']
 
 directions.forEach((direction) => {
   $('#' + direction).addEventListener('click', () => {
     console.log(`${direction} clicked!`)
 
     room.send({ move: direction })
+  })
+})
+
+document.body.appendChild(app.view)
+
+PIXI.loader.add('sprite', assets.sprite).load((loader, resources) => {
+  const sprite = new PIXI.Sprite(resources.sprite.texture)
+
+  sprite.x = app.renderer.width / 2
+  sprite.y = app.renderer.height / 2
+  sprite.anchor.x = 0.5
+  sprite.anchor.y = 0.5
+
+  app.stage.addChild(sprite)
+  app.ticker.add(() => {
+    sprite.rotation += 0.01
   })
 })
