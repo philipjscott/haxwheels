227312a05b16b74b54cc4626625ba456c7aac237
Philip Scott <pscott@zeptohost.com>
2018-07-15 23:29:06 -0400

    feat(25): Add client-side interpolation

--- client/view.js
+++ client/view.js
@@ -2,33 +2,48 @@
 
 import * as PIXI from 'pixi.js'
 import * as assets from './assets'
+import { cosineInterp } from './interp'
 
 class View {
   constructor () {
     this._app = new PIXI.Application()
-    this._players = {}
+    this._sprites = {}
+    this._positions = {}
 
     document.body.appendChild(this._app.view)
+    this._renderLoop()
+  }
+
+  _renderLoop () {
+    this._app.ticker.add((delta) => {
+      for (const id in this._positions) {
+        const sprite = this._sprites[id]
+        const position = this._positions[id]
+
+        console.log(delta)
+
+        sprite.x = cosineInterp(sprite.x, position.x, delta)
+        sprite.y = cosineInterp(sprite.y, position.y, delta)
+      }
+    })
   }
 
   _createPlayer (id, position) {
     const sprite = PIXI.Sprite.fromImage(assets.sprite)
 
-    sprite.x = position.x
-    sprite.y = position.y
-
     this._app.stage.addChild(sprite)
-    this._players[id] = sprite
+    this._positions[id] = position
+    this._sprites[id] = sprite
   }
 
   _removePlayer (id) {
-    this._players[id].destroy()
+    this._sprites[id].destroy()
+    delete this._sprites[id]
+    delete this._positions[id]
   }
 
   _updatePosition (id, coord, value) {
-    const sprite = this._players[id]
-
-    sprite[coord] = value
+    this._positions[id][coord] = value
   }
 
   updatePlayer (change) {
--- game/rooms/race.js
+++ game/rooms/race.js
@@ -15,7 +15,7 @@ class RaceRoom extends Room {
   }
 
   onMessage (client, data) {
-    const step = 5
+    const step = 40
     const player = this.state.players[client.sessionId]
 
     console.log('Message received!', data)
